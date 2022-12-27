9749814132bdfa6e0b1e8c451ad900217236fa15
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-27 14:02:44 +0530

    Destructuring Arrays and Objects

--- 08_ReactjsEssentialTraining/react-app/src/App.js
+++ 08_ReactjsEssentialTraining/react-app/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const [firstCity, second] = ["Tokyo", "Tahoe City", "Bend"];
+console.log(firstCity)
+console.log(second)
+
+function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from react</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
