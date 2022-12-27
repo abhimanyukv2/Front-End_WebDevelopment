8dec4e46c09b282f8e7008791f5b1244880202e1
Abhimanyu Kumar <abhimanyukv2@gmail.com>
2022-12-27 16:20:32 +0530

    Working with UseEffect

--- 08_ReactjsEssentialTraining/react-app/src/App.js
+++ 08_ReactjsEssentialTraining/react-app/src/App.js
@@ -1,6 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const [firstCity, second] = ["Tokyo", "Tahoe City", "Bend"];
 console.log(firstCity)
@@ -8,6 +8,11 @@ console.log(second)
 
 function App({ library }) {
   const [emotion, setEmotion] = useState("happy");
+
+  useEffect(() =>{
+    console.log(`It's ${emotion} right now`);
+  }, [emotion]);
+  
   return (
     <div className="App">
       <h1>Hello from {library}</h1>
